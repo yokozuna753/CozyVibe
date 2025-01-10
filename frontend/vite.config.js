3b8f97ff6261dbef2d4e8a6b6e6a775464a63830
yokozuna753 <brian894526@gmail.com>
2025-01-10 10:05:00 -0500

    added proxy to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -8,12 +8,15 @@ export default defineConfig(({ mode }) => ({
     react(),
     eslint({
       lintOnStart: true,
-      failOnError: mode === "production"
-    })
+      failOnError: mode === "production",
+    }),
   ],
   // To automatically open the app in the browser whenever the server starts,
   // uncomment the following lines:
   server: {
-    open: true
-  }
+    open: true,
+    proxy: {
+      "/api": "http://localhost:8000",
+    },
+  },
 }));
