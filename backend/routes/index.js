be4efc0d36e76cb7d6959b1520ca9d7a9aaa377a
yokozuna753 <brian894526@gmail.com>
2025-01-11 13:13:55 -0500

    added dist folder and ran build command

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -31,7 +31,8 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the static assets in the frontend's build folder
   router.use(express.static(path.resolve("../frontend/dist")));
 
-  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  // Serve the frontend's index.html file at all other routes 
+  // NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
@@ -46,25 +47,16 @@ if (process.env.NODE_ENV === 'production') {
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
   });
 }
 
 // ...
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
